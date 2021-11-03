3bed05a54d4e175ca0759613f28b145532e685db
RodrighoNS <rnavarro@lemontech.com>
2021-11-03 19:45:57 -0300

    Refactor to expose the api

--- server/main.go
+++ server/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	fmt.Println("Server started")
-	log.Fatal(http.ListenAndServe(":8085", nil))
+	router := gin.Default()
+	router.GET("/devs", getDevs)
+
+	router.Run(":8085")
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message":"Hola vecinillo!"}`))
+// deb represents data about a Dev
+type dev struct {
+	ID   string `json:"id"`
+	name string `json:"name"`
+	role string `json:"role"`
+}
+
+// devs slice to seed record dev data.
+var devs = []dev{
+	{ID: "1", name: "Josefina J", role: "frontend dev"},
+}
+
+// getDevs responds with the list of all Devs as JSON
+func getDevs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, devs)
 }
